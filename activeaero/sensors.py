a1a46f34511d3ee1095de363e9f0aa42492334ff
Alex <alex.k.korte@gmail.com>
2023-01-19 14:03:35 -0500

    Update sensors.py

--- activeaero/sensors.py
+++ activeaero/sensors.py
@@ -6,7 +6,7 @@ import adafruit_bmp3xx
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sox = LSM6DSOX(i2c)
-mag = LIS3MDL(i2c, address=0x1E)
+# mag = LIS3MDL(i2c, address=0x1E)
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
 def get_sensor_data():
